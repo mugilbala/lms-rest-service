ec29a170ceafe29a00ef01876a02831f439ab48d
Anbu <mugilbala@gmail.com>
2022-03-30 22:32:31 -0400

    Added swagger.

--- src/main/java/com/ninja/lms/LmsRestServiceApplication.java
+++ src/main/java/com/ninja/lms/LmsRestServiceApplication.java
@@ -2,8 +2,10 @@ package com.ninja.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LmsRestServiceApplication {
 
     public static void main(String[] args) {
